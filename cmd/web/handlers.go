a5e0e449d8b04526d6951b54606f6fa1ea0dbe56
Vatt Sopheak <vattcarter@gmail.com>
2023-03-27 19:33:14 +0700

    Added form validation

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -45,6 +45,14 @@ func (app *application) Login(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// validate data
+	form := NewForm(r.PostForm)
+	form.Required("email", "password")
+
+	if !form.Valid() {
+		fmt.Fprint(w, "failed validation")
+	}
+
 	email := r.Form.Get("email")
 	password := r.Form.Get("password")
 
