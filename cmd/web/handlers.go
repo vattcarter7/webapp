c0308bd5b2a9b52a4c5608f39a063f65bc4e7324
Vatt Sopheak <vattcarter@gmail.com>
2023-03-27 12:54:00 +0700

    Initial commit

---
+++ cmd/web/handlers.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+	"path"
+)
+
+var pathToTemplates = "./templates/"
+
+func (app *application) Home(w http.ResponseWriter, r *http.Request) {
+	_ = app.render(w, r, "home.page.gohtml", &TemplateData{})
+}
+
+type TemplateData struct {
+	IP   string
+	Data map[string]any
+}
+
+func (app *application) render(w http.ResponseWriter, r *http.Request, t string, data *TemplateData) error {
+	// parse the template from disk.
+	parsedTemplate, err := template.ParseFiles(path.Join(pathToTemplates, t))
+	if err != nil {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return err
+	}
+
+	// execute the template, passing it's data, if any
+	err = parsedTemplate.Execute(w, data)
+	if err != nil {
+		return err
+	}
+	return nil
+}
