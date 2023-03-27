78ed33b521236bacafa050537aec7e17e2de8b18
Vatt Sopheak <vattcarter@gmail.com>
2023-03-27 13:53:50 +0700

    Added Login stub handler

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 	"path"
 )
@@ -33,4 +35,20 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, t string,
 		return err
 	}
 	return nil
+}
+
+func (app *application) Login(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return
+	}
+
+	email := r.Form.Get("email")
+	password := r.Form.Get("password")
+
+	log.Println(email, password)
+
+	fmt.Fprint(w, email)
 }
