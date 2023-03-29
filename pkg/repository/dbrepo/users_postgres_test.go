00bb2530d7ec5df647d04e0b87552fc0e4a33c2f
Vatt Sopheak <vattcarter@gmail.com>
2023-03-29 18:40:02 +0700

    Implemented testing for GetUser and GetUserByEmail

--- pkg/repository/dbrepo/users_postgres_test.go
+++ pkg/repository/dbrepo/users_postgres_test.go
@@ -154,7 +154,7 @@ func TestPostgresDBRepoAllUsers(t *testing.T) {
 	testUser := data.User{
 		FirstName: "Jack",
 		LastName:  "Smith",
-		Email:     "admin@example.com",
+		Email:     "jack@smith.com",
 		Password:  "secret",
 		IsAdmin:   1,
 		CreatedAt: time.Now(),
@@ -170,5 +170,32 @@ func TestPostgresDBRepoAllUsers(t *testing.T) {
 
 	if len(users) != 2 {
 		t.Errorf("all users reports wrong size after insert; expected 2, but got %d", len(users))
+	}
+}
+
+func TestPostgresDBRepo(t *testing.T) {
+	user, err := testRepo.GetUser(1)
+	if err != nil {
+		t.Errorf("error getting user by id: %s", err)
+	}
+
+	if user.Email != "admin@example.com" {
+		t.Errorf("wrong email returned by GetUser; expected admin@example.com but got %s", user.Email)
+	}
+
+	_, err = testRepo.GetUser(3)
+	if err == nil {
+		t.Error("no error reported when getting non existing user by id")
+	}
+}
+
+func TestPostgresDBRepoGetUserByEmail(t *testing.T) {
+	user, err := testRepo.GetUserByEmail("jack@smith.com")
+	if err != nil {
+		t.Errorf("error getting user by email: %s", err)
+	}
+
+	if user.ID != 2 {
+		t.Errorf("wrong id returned by GetUserByEmail; expected 2 but got %d", user.ID)
 	}
 }
