1d708ebe4cffadb0c86ab2c4f17b87030f9a91a7
Vatt Sopheak <vattcarter@gmail.com>
2023-03-31 11:54:52 +0700

    Testing upload handler

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -14,6 +14,7 @@ import (
 )
 
 var pathToTemplates = "./templates/"
+var uploadPath = "./static/img"
 
 func (app *application) Home(w http.ResponseWriter, r *http.Request) {
 	var td = make(map[string]any)
@@ -51,6 +52,10 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, t string,
 
 	td.Error = app.Session.PopString(r.Context(), "error")
 	td.Flash = app.Session.PopString(r.Context(), "flash")
+
+	if app.Session.Exists(r.Context(), "user") {
+		td.User = app.Session.Get(r.Context(), "user").(data.User)
+	}
 
 	// execute the template, passing it's data, if any
 	err = parsedTemplate.Execute(w, td)
@@ -115,16 +120,39 @@ func (app *application) authenticate(r *http.Request, user *data.User, password 
 
 func (app *application) UploadProfilePic(w http.ResponseWriter, r *http.Request) {
 	// call a function that extracts a file from an upload (request)
+	files, err := app.UploadFiles(r, uploadPath)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
 
 	// get the user from the session
+	user := app.Session.Get(r.Context(), "user").(data.User)
 
 	// create a var of type data.UserImage
+	var i = data.UserImage{
+		UserID:   user.ID,
+		FileName: files[0].OriginalFileName,
+	}
 
 	// insert the user image into user_images
+	_, err = app.DB.InsertUserImage(i)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
 
 	// refresh the sessional variable "user"
+	updatedUser, err := app.DB.GetUser(user.ID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	app.Session.Put(r.Context(), "user", updatedUser)
 
 	// redirect back to profile page
+	http.Redirect(w, r, "/user/profile", http.StatusSeeOther)
 }
 
 type UploadedFile struct {
