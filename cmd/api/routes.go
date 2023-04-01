183b850ec263fb7356c7f2c1a05da6aea1cfeff2
Vatt Sopheak <vattcarter@gmail.com>
2023-04-01 13:12:28 +0700

    Added endpoints for API

---
+++ cmd/api/routes.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func (app *application) routes() http.Handler {
+	mux := chi.NewRouter()
+
+	// register middleware
+	mux.Use(middleware.Recoverer)
+	// mux.Use(app.enableCORS)
+
+	// authentication routes - auth handler, refresh
+	mux.Post("/auth", app.authenticate)
+	mux.Post("/refresh-token", app.refresh)
+
+	// test handler
+
+	// protected routes
+	mux.Route("/users", func(mux chi.Router) {
+		// use auth middleware
+
+		mux.Get("/", app.allUsers)
+		mux.Get("/{userID}", app.getUser)
+		mux.Delete("/{userID}", app.deleteUser)
+		mux.Put("/", app.insertUser)
+		mux.Patch("/", app.updateUser)
+	})
+
+	return mux
+}
